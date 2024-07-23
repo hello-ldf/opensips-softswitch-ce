9f94d7cf0e833d299852b9fdd9806c314bf145f1
Stefan Darius <94108614+dariusstefan@users.noreply.github.com>
2024-07-23 15:18:32 +0300

    Add default dialplan.

---
+++ etc/opensips-cp/docker-entrypoint.d/10_config_dialplan.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Configuring OpenSIPS CP Dialplan Tool ..."
+
+mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE} -e \
+	"INSERT INTO dialplan (dpid, match_op, match_exp)
+	 SELECT '${DEFAULT_DIALPLAN}', 1, '${DEFAULT_REGEX}'
+		 WHERE NOT EXISTS (
+		 SELECT 1 FROM dialplan WHERE dpid = '${DEFAULT_DIALPLAN}' AND match_op = 1 AND match_exp = '${DEFAULT_REGEX}');"
