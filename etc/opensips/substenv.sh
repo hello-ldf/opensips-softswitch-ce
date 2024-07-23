d782ad4d18f4dcc79f9ad0eb93df28d0a43ade71
Razvan Crainea <razvan@opensips.org>
2024-07-23 09:33:29 +0300

    opensips: remove static config file

--- etc/opensips/substenv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))"
-
-tempfile=$(mktemp)
-envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < /etc/opensips/opensips-cli.cfg > "$tempfile"
-
-mv "$tempfile" /etc/opensips/opensips-cli.cfg
