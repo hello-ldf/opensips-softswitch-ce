5024df0487d83f7c3af9d298d36214495c7cc198
Stefan Darius <94108614+dariusstefan@users.noreply.github.com>
2024-07-23 11:28:26 +0300

    config: Create add_column function

--- etc/opensips-cp/docker-entrypoint.d/10_config_user_management.sh
+++ etc/opensips-cp/docker-entrypoint.d/10_config_user_management.sh
@@ -13,11 +13,19 @@ mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE}
 cp -u /docker-entrypoint.d/user_management/acls.inc.php /var/www/html/opensips-cp/config/tools/users/user_management/
 cp -u /docker-entrypoint.d/user_management/user_management_acls.php /var/www/html/opensips-cp/web/tools/users/user_management/
 
-ACL_COLUMN_EXISTS=$(mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE} -e \
-	"SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'subscriber' AND COLUMN_NAME = 'acls';")
+add_column() {
+    table_name=$1
+    column_name=$2
+    column_options=$3
 
-[ -z "$ACL_COLUMN_EXISTS" ] && mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE} -e \
-	"ALTER TABLE subscriber ADD COLUMN acls CHAR(64) DEFAULT '' NOT NULL;"
+    COLUMN_EXISTS=$(mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE} -e \
+        "SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '${table_name}' AND COLUMN_NAME = '${column_name}';")
+
+    [ -z "$COLUMN_EXISTS" ] && mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE} -e \
+        "ALTER TABLE ${table_name} ADD COLUMN ${column_name} ${column_options};"
+}
+
+add_column subscriber acls 'VARCHAR(64) DEFAULT "" NOT NULL'
 
 mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE} -e \
 	"INSERT INTO ocp_tools_config (module, param, value)
