2a9789b4ffbf69d5ab5e457f92a70df8c199fbe5
Stefan Darius <94108614+dariusstefan@users.noreply.github.com>
2024-07-25 12:24:58 +0300

    Add a list with sql files to run on db.

--- etc/opensips-cp/docker-entrypoint.d/00_config_db.sh
+++ etc/opensips-cp/docker-entrypoint.d/00_config_db.sh
@@ -3,6 +3,43 @@
 cd "$(dirname "$0")"
 . functions
 
+TABLE_FILES=(
+    "acc-create.sql"
+    "alias_db-create.sql"
+    "auth_db-create.sql"
+    "authjwt-create.sql"
+    "b2b-create.sql"
+    "b2b_sca-create.sql"
+    "cachedb_sql-create.sql"
+    "carrierroute-create.sql"
+    "cpl-create.sql"
+    "db-migrate.sql"
+    "dialog-create.sql"
+    "dialplan-create.sql"
+    "dispatcher-create.sql"
+    "domain-create.sql"
+    "domainpolicy-create.sql"
+    "drouting-create.sql"
+    "emergency-create.sql"
+    "fraud_detection-create.sql"
+    "freeswitch_scripting-create.sql"
+    "group-create.sql"
+    "imc-create.sql"
+    "msilo-create.sql"
+    "presence-create.sql"
+    "qrouting-create.sql"
+    "ratecacher-create.sql"
+    "registrant-create.sql"
+    "rls-create.sql"
+    "rtpproxy-create.sql"
+    "speeddial-create.sql"
+    "sqlops-create.sql"
+    "table-migrate.sql"
+    "tracer-create.sql"
+    "userblacklist-create.sql"
+    "usrloc-create.sql"
+)
+
 echo "Configuring OpenSIPS-CP database connection..."
 
 sed -i "s/localhost/${MYSQL_IP}/g" /var/www/html/opensips-cp/config/db.inc.php
@@ -25,8 +62,8 @@ add_table /usr/share/opensips/mysql/standard-create.sql
 
 rm -f /usr/share/opensips/mysql/standard-create.sql
 
-for file in /usr/share/opensips/mysql/*.sql; do
-    add_table $file
+for file in "${TABLE_FILES[@]}"; do
+    add_table /usr/share/opensips/mysql/$file
 done
 
 add_table /var/www/html/opensips-cp/config/db_schema.mysql
