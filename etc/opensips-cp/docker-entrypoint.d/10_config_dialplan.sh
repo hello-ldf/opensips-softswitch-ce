ce169e18f692c08a8371fe7307067e9641bcaf73
Stefan Darius <94108614+dariusstefan@users.noreply.github.com>
2024-07-30 12:37:05 +0300

    Add atribute DP_USER for default dp.

--- etc/opensips-cp/docker-entrypoint.d/10_config_dialplan.sh
+++ etc/opensips-cp/docker-entrypoint.d/10_config_dialplan.sh
@@ -3,7 +3,7 @@
 echo "Configuring OpenSIPS CP Dialplan Tool ..."
 
 mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE} -e \
-	"INSERT INTO dialplan (dpid, match_op, match_exp)
-	 SELECT '${DEFAULT_DIALPLAN}', 1, '${DEFAULT_REGEX}'
+	"INSERT INTO dialplan (dpid, match_op, match_exp, attrs)
+	 SELECT '${DEFAULT_DIALPLAN}', 1, '${DEFAULT_REGEX}', 'DP_USER'
 		 WHERE NOT EXISTS (
 		 SELECT 1 FROM dialplan WHERE dpid = '${DEFAULT_DIALPLAN}' AND match_op = 1 AND match_exp = '${DEFAULT_REGEX}');"
