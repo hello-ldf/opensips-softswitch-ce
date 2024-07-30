b42d71b5c050e4b2ea74d6cb173fe6d01b49b512
Razvan Crainea <razvan@opensips.org>
2024-07-30 14:27:41 +0300

    db/dialplan: add default dialplan rules

--- etc/opensips-cp/docker-entrypoint.d/10_config_dialplan.sh
+++ etc/opensips-cp/docker-entrypoint.d/10_config_dialplan.sh
@@ -2,8 +2,24 @@
 
 echo "Configuring OpenSIPS CP Dialplan Tool ..."
 
-mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE} -e \
-	"INSERT INTO dialplan (dpid, match_op, match_exp, attrs)
-	 SELECT '${DEFAULT_DIALPLAN}', 1, '${DEFAULT_REGEX}', 'DP_USER'
-		 WHERE NOT EXISTS (
-		 SELECT 1 FROM dialplan WHERE dpid = '${DEFAULT_DIALPLAN}' AND match_op = 1 AND match_exp = '${DEFAULT_REGEX}');"
+add_dp_rule() {
+	local attr="$1"
+	local regex="$2"
+	local subst="$3"
+	local repl="$4"
+
+	[ -n "$subst" ] && subst="'$subst'" || subst=NULL
+	[ -n "$repl" ] && repl="'$repl'" || repl=NULL
+
+	mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D"${MYSQL_DATABASE}" -e \
+		"INSERT INTO dialplan (dpid, match_op, match_exp, subst_exp, repl_exp, attrs)
+		 SELECT 1, 1, '${regex}', ${subst}, ${repl}, '${attr}'
+			 WHERE NOT EXISTS (
+			 SELECT 1 FROM dialplan WHERE dpid = 1 AND attrs = '${attr}');"
+}
+
+add_dp_rule DP_DID '^(\\+|00|011)?[1-9][0-9]{6,}$' '^(\\+|00|011)?([1-9][0-9]{6,})$' '\\2'
+add_dp_rule DP_USER '^[a-zA-Z][a-zA-Z0-9\\-_\\.]{4,}$'
+add_dp_rule DP_DND_ON '^\\*78$'
+add_dp_rule DP_DND_OFF '^\\*79$'
+add_dp_rule DP_VMLISTEN '^\\*98$'
