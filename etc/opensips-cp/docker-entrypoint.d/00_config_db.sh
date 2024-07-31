461ae066f91ff5bf81f03a1560f58f8712a6d0e1
Razvan Crainea <razvan@opensips.org>
2024-07-31 09:28:10 +0300

    avoid cross-dependencies not listed in docker-compose
    The database should be fully functional between opensips and/or
    opensips-cp use it - we should not create artificial dependencies in the
    init script

--- etc/opensips-cp/docker-entrypoint.d/00_config_db.sh
+++ etc/opensips-cp/docker-entrypoint.d/00_config_db.sh
@@ -9,16 +9,4 @@ sed -i "s/localhost/${MYSQL_IP}/g" /var/www/html/opensips-cp/config/db.inc.php
 
 sed -i "s/127.0.0.1/${OPENSIPS_IP}/g" /var/www/html/opensips-cp/config/db_schema.mysql
 
-apt-get update && apt-get install -y curl
-
-curl https://apt.opensips.org/opensips-org.gpg -o /usr/share/keyrings/opensips-org.gpg
-echo "deb [signed-by=/usr/share/keyrings/opensips-org.gpg] https://apt.opensips.org bullseye 3.6-nightly" >/etc/apt/sources.list.d/opensips.list
-
-apt-get update && apt-get install -y opensips-mysql-dbschema
-
-until mysqladmin ping -h ${MYSQL_IP} --silent; do
-    echo "MySQL server is not yet available. Retrying in 1 second..."
-    sleep 1
-done
-
 add_table /var/www/html/opensips-cp/config/db_schema.mysql
