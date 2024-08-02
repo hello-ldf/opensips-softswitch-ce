14f8840b67e6d1069284e87c8fc724935452db8b
Stefan Darius <94108614+dariusstefan@users.noreply.github.com>
2024-08-02 17:36:08 +0300

    Update mysql-client scripts to cleaner solutions.

--- sipssert/02.call_disabled_caller/scripts/disable_user.sh
+++ sipssert/02.call_disabled_caller/scripts/disable_user.sh
@@ -1,11 +1,1 @@
-mysql opensips -e "UPDATE subscriber SET acls = '' WHERE username = '$1' AND domain = '$2';"
-
-result=$(mysql opensips -se "SELECT acls FROM subscriber WHERE username = '$1' AND domain = '$2';")
-
-if [[ "$result" == "" ]]; then
-    echo "ACL DISABLED"
-    exit 0
-else
-    echo "ACL ENABLED"
-    exit 1
-fi
+mysql opensips -e "UPDATE subscriber SET acls = REPLACE(acls, 'E', '') WHERE username = '$1' AND domain = '$2';"
--- sipssert/init_tasks/enable_user.sh
+++ sipssert/init_tasks/enable_user.sh
@@ -1,11 +1,1 @@
-mysql opensips -e "UPDATE subscriber SET acls = 'E' WHERE username = '$1' AND domain = '$2';"
-
-result=$(mysql opensips -se "SELECT acls FROM subscriber WHERE username = '$1' AND domain = '$2';")
-
-if [[ "$result" == "E" ]]; then
-    echo "ACL ENABLED"
-    exit 0
-else
-    echo "ACL NOT ENABLED"
-    exit 1
-fi
+mysql opensips -e "UPDATE subscriber SET acls = CONCAT(acls, 'E') WHERE username = '$1' AND domain = '$2';"
