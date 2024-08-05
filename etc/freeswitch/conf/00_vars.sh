aef3375fd0ccb2afe92b337067a105ddc35d2a9c
Stefan Darius <94108614+dariusstefan@users.noreply.github.com>
2024-08-05 14:47:38 +0300

    freeswitch: change external_rtp_ip

--- etc/freeswitch/conf/00_vars.sh
+++ etc/freeswitch/conf/00_vars.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 eval "sed -i 's/external_sip_ip=stun:stun.freeswitch.org/external_sip_ip=${FREESWITCH_IP}/' /etc/freeswitch/vars.xml"
+eval "sed -i 's/external_rtp_ip=stun:stun.freeswitch.org/external_rtp_ip=${FREESWITCH_IP}/' /etc/freeswitch/vars.xml"
