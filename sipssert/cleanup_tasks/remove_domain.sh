e7dc8621e7cd84f5bdd795918822f9e2a5be94c7
Stefan Darius <94108614+dariusstefan@users.noreply.github.com>
2024-08-05 13:07:16 +0300

    sipssert: Simplify domain add/remove scripts.

--- sipssert/cleanup_tasks/remove_domain.sh
+++ sipssert/cleanup_tasks/remove_domain.sh
@@ -1,11 +1,1 @@
 mysql opensips -e "DELETE FROM domain WHERE domain = '$1';"
-
-result=$(mysql opensips -se "SELECT domain FROM domain WHERE domain = '$1';")
-
-if [[ "$result" == "$1" ]]; then
-    echo "DOMAIN NOT REMOVED"
-    exit 1
-else
-    echo "DOMAIN REMOVED"
-    exit 0
-fi
--- sipssert/init_tasks/add_domain.sh
+++ sipssert/init_tasks/add_domain.sh
@@ -1,11 +1,1 @@
 mysql opensips -e "INSERT INTO domain (domain) VALUES ('$1');"
-
-result=$(mysql opensips -se "SELECT domain FROM domain WHERE domain = '$1';")
-
-if [[ "$result" == "$1" ]]; then
-    echo "DOMAIN ADDED"
-    exit 0
-else
-    echo "DOMAIN NOT ADDED"
-    exit 1
-fi
