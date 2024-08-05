91825876774577350fe4f5256c2157e87cb76ceb
Stefan Darius <94108614+dariusstefan@users.noreply.github.com>
2024-08-05 15:13:41 +0300

    freeswitch: fix vars.xml edit script

--- etc/freeswitch/conf/00_vars.sh
+++ etc/freeswitch/conf/00_vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-eval "sed -i 's/external_sip_ip=stun:stun.freeswitch.org/external_sip_ip=${FREESWITCH_IP}/' /etc/freeswitch/vars.xml"
-eval "sed -i 's/external_rtp_ip=stun:stun.freeswitch.org/external_rtp_ip=${FREESWITCH_IP}/' /etc/freeswitch/vars.xml"
+eval "sed -E -i 's/external_sip_ip=[^\"]+/external_sip_ip=${FREESWITCH_IP}/' /etc/freeswitch/vars.xml"
+eval "sed -E -i 's/external_rtp_ip=[^\"]+/external_rtp_ip=${FREESWITCH_IP}/' /etc/freeswitch/vars.xml"
