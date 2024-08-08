8f4c202aa743e80068d40cbb64acdccb68398bd9
Razvan Crainea <razvan@opensips.org>
2024-08-08 18:21:57 +0300

    run-sipssert: pass additional params to sipssert

--- run-sipssert.sh
+++ run-sipssert.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 TESTS_DIR=${1:-sipssert}
+test -n "$1" && shift
 
 docker run --rm -it \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v "$(pwd)/${TESTS_DIR}":"$(pwd)/${TESTS_DIR}" \
 	-w "$(pwd)/${TESTS_DIR}" \
-	opensips/sipssert .
+	opensips/sipssert . $@
