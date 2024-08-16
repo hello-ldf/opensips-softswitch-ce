af3cb6d61daf690571e8ca4bb5b3078583486159
Stefan Darius <94108614+dariusstefan@users.noreply.github.com>
2024-08-16 16:03:23 +0300

    opensips-cp: change HEX to explicit strings

--- etc/opensips-cp/docker-entrypoint.d/30_config_drouting.sh
+++ etc/opensips-cp/docker-entrypoint.d/30_config_drouting.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")"
 
 echo "Configuring OpenSIPS CP Dynamic Routing Tool..."
 
-add_parameter drouting group_ids_file "UNHEX('7B2231223A2244656661756C74227D')"
-add_parameter drouting tabs "UNHEX('67617465776179732E7068702C63617272696572732E7068702C72756C65732E706870')"
+add_parameter drouting tabs '"gateways.php,carriers.php,rules.php"'
+add_parameter drouting group_ids_file "'{\"1\":\"Default\"}'"
