9204f9c4d0ed4a5f79e6543f838243195ae0d10b
Stefan Darius <94108614+dariusstefan@users.noreply.github.com>
2024-08-20 17:04:52 +0300

    use new getenv filter in defines.yml

--- run-sipssert.sh
+++ run-sipssert.sh
@@ -4,6 +4,7 @@ TESTS_DIR=${1:-sipssert}
 test -n "$1" && shift
 
 docker run --rm -it \
+    --env-file .env \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v "$(pwd)/${TESTS_DIR}":"$(pwd)/${TESTS_DIR}" \
 	-w "$(pwd)/${TESTS_DIR}" \
